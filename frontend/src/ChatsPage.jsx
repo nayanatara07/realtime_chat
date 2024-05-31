b623d4c1779d0b2e1e0d8ee9179c88bd1725411e
nayanatara07 <nayana2017tara@gmail.com>
2024-05-31 10:43:53 +0530

    frontend done

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,6 +1,19 @@
-const ChatsPage = () => {
-    return <>chats...</>
-  }
-export default ChatsPage
-  
-  
+import React from 'react';
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced';
+
+const ChatsPage = ({ user }) => {
+    const chatProps = useMultiChatLogic(
+        process.env.REACT_APP_PRIVATE_KEY,
+        user.username,
+        user.secret
+    );
+
+    return (
+        <div style={{ height: '100vh' }}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow {...chatProps} style={{ height: '100%' }} />
+        </div>
+    );
+};
+
+export default ChatsPage;
